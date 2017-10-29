d7d1c5765106db89d4753a9d74c8d89d0afa5c40
Alberto Villanueva Nieto <32910015+AlbertoVillanueva@users.noreply.github.com>
2017-10-29 17:01:10 +0100

    Update assemblycode.s
    OCD

--- assemblycode.s
+++ assemblycode.s
@@ -14,10 +14,10 @@ main:			#abrimos el fichero
 				li $t0 -1
 				beq $v0 $t0 error
 				
-				#copiamos el descriptor del fichero						$t0 descriptor del archivo
+			#copiamos el descriptor del fichero						$t0 descriptor del archivo
 				move $t0 $v0
 				
-				#inicializamos el contador a 0							$t3 contador
+			#inicializamos el contador a 0							$t3 contador
 				li $t3 0
 				
 bucle:			#lee 1 caracter
@@ -27,7 +27,7 @@ bucle:			#lee 1 caracter
 				li $v0 14
 				syscall
 				
-				#comprobamos si es un espacio, un tabulado, o un salto de linea		$t2 caracter en buffer
+			#comprobamos si es un espacio, un tabulado, o un salto de linea			$t2 caracter en buffer
 				lw $t2 buffer
 				
 				li $t1 9
@@ -37,29 +37,30 @@ bucle:			#lee 1 caracter
 				li $t1 20
 				beq $t1 $t2 bucle
 				
-				#si no lo es seguimos
-				#paso el caracter leido como parametro
+			#si no lo es seguimos
+			#pasamos el caracter leido como parametro
 				move $a0 $t2
 				
-				#guardamos en la pila el $ra y el $fp
+			#guardamos en la pila el $ra y el $fp
 				addi $sp $sp -8
 				sw $ra 4($sp)
 				sw $fp ($sp)
 				
-				#llamamos a la funcion
+			#llamamos a la funcion
 				jal haySubcadena
 				
-				#recuperamos el $fp y el $ra
+			#recuperamos el $fp y el $ra
 				lw $fp ($sp)
 				lw $ra 4($sp)
 				addi $sp $sp 8
 				
-				#sumamos al contador si hay subcadena
+			#sumamos al contador si hay subcadena
 				add $t3 $t3 $v1
 				
-				#comprobamos si en el subprograma o en el descarte de espacios ha llegado al final
+			#comprobamos si en el subprograma o en el descarte de espacios ha llegado al final
 				bnez $v0 bucle
 				
-error:			lw $a0 	mError
+error:			#sacamos por pantalla el mensaje de error
+				la $a0 	mError
 				li $v0 4
 				syscall
