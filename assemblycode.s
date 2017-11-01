035913ac13f112dc806b4e5744c9ecbce6288d7b
Alberto Villanueva Nieto <32910015+AlbertoVillanueva@users.noreply.github.com>
2017-11-01 20:04:50 +0100

    Update assemblycode.s
    -añadido más comentarios
    -renombrado algunas variables
    -solucionado algun pequeño error
    4 space >>>> 8 space

--- assemblycode.s
+++ assemblycode.s
@@ -13,11 +13,11 @@ main:			#abrimos el fichero
 				syscall
 				li $t0 -1
 				beq $v0 $t0 error
-				
-			#copiamos el descriptor del fichero						$s0 descriptor del archivo
+			
+			#copiamos el descriptor del fichero										$s0 descriptor del archivo
 				move $s0 $v0
 				
-			#inicializamos el contador a 0							$s1 contador
+			#inicializamos el contador a 0											$s1 contador
 				li $s1 0
 				
 bucle:			#lee 1 caracter
@@ -26,20 +26,23 @@ bucle:			#lee 1 caracter
 				li $a2 1
 				li $v0 14
 				syscall
+			
+			#si ha terminado el archivo terminamos el programa
+				beqz $v0 finPrograma
 				
-			#comprobamos si es un espacio, un tabulado, o un salto de linea			$t2 caracter en buffer
-				lw $t2 buffer
+			#comprobamos si es un espacio, un tabulado, o un salto de linea			$t0 caracter en buffer
+				lb $t0 buffer
 				
-				li $t1 9
-				beq $t1 $t2 bucle
-				li $t1 13
-				beq $t1 $t2 bucle
-				li $t1 20
-				beq $t1 $t2 bucle
+				li $t1 0x9
+				beq $t1 $t0 bucle
+				li $t1 0x13
+				beq $t1 $t0 bucle
+				li $t1 0x20
+				beq $t1 $t0 bucle
 				
 			#si no lo es seguimos
 			#pasamos el caracter leido como parametro
-				move $a0 $t2
+				move $a0 $t0
 				
 			#guardamos en la pila el $ra y el $fp
 				addi $sp $sp -8
@@ -53,7 +56,7 @@ bucle:			#lee 1 caracter
 				lw $fp ($sp)
 				lw $ra 4($sp)
 				addi $sp $sp 8
-				
+			
 			#sumamos al contador si hay subcadena
 				add $s1 $s1 $v1
 				
@@ -63,8 +66,7 @@ bucle:			#lee 1 caracter
 			#cerramos el archivo y terminamos el programa
 				move $a0 $s0
 				li $v0 16
-				syscall
-				
+				syscall					
 				b finPrograma
 				
 error:			#sacamos por pantalla el mensaje de error
@@ -73,59 +75,63 @@ error:			#sacamos por pantalla el mensaje de error
 				syscall
 				b finPrograma
 				
-haySubcadena:		#nuestra funcion
+haySubcadena:	
+			#nuestra funcion
 			#iniciamos variables
-				li $t0 0							#$t0 contador de substring
-				li $v0 0							#$v0 valor de retorno
-			#cargamos el primer byte de la subcadena en $t4, si no es igual saltamos al else, si es igual sumamos uno al contador
-				lb $t4 cadena
-				bne $a0 $t4 else
-				addi $t0 $t0 1
+				li $t0 0															#$t0 contador de substring
+				li $v1 0															#$v1 valor de retorno
+			#empezamos el bucle habiendo leido un caracter
+				move $t1 $a0
+				b despuesDeLeer
 				
-			#inicializamos el valor de retorno a 0	
-bucleChar:		#leemos otro caracter
+bucleChar:		#leemos un caracter
 				move $a0 $s0
 				la $a1 buffer
 				li $a2 1
 				li $v0 14
 				syscall
-				
+			
 			#si ha llegado al final del archivo terminamos la funcion
 				beqz $v0 fin
+			
+			#guardamos el carater leido												#$t1 caracter leido del archivo
+				lb $t1 buffer
 				
-				lb $t3 buffer							#$t3 caracter leido del archivo
-				
-			#si no es igual al primer caracter de la subcadena 
-				lb $t5 cadena($t0)						#$t5 caracter actual de la subcadena
-				bne $t3 $t5 else
+despuesDeLeer:	
+			#si no es igual al caracter actual de la subcadena saltamos a else
+			#si es igual al primer caracter de la subcadena sumamos 1 al contador de la subcadena y volvemos a empezar el bucle
+				lb $t2 cadena($t0)													#$t2 caracter actual de la subcadena
+				bne $t1 $t2 else	
 				addi $t0 $t0 1
 				b bucleChar
 				
-else:			#si es el final de la subcadena sumamos 1 a v0(que es lo que devolvemos) y salimos del bucle 
-				lb $t5 cadena($t0)						#$t5 caracter actual de la subcadena
-				bnez $t5 noFinalSubcadena
+else:		#si es el final de la subcadena ponemos 1 en v1 (que es lo que devolvemos) y salimos del bucle
+			#si no, saltamos a noFinalSubcadena
+				bnez $t2 noFinalSubcadena
 				li $v1 1
-				b fin
+				b irFinalPalabra
 				
-noFinalSubcadena:	#si el ultimo caracter leido es un espacio, un tabulado o un enter salimos de bucle
-				li $t2 9
-				beq $t3 $t2 fin
-				li $t2 13
-				beq $t3 $t2 fin
-				li $t2 20
-				beq $t3 $t2 fin
+
+noFinalSubcadena:
+			#si el ultimo caracter leido es un espacio, un tabulado o un enter salimos de bucle
+				li $t3 0x9
+				beq $t1 $t3 fin
+				li $t3 0x13
+				beq $t1 $t3 fin
+				li $t3 0x20
+				beq $t1 $t3 fin
 				
 			#si no es el final, empezamos de nuevo en busca de la subcadena	
 				li $t0 0				
 				b bucleChar	
 				
-irFinalPalabra:		#leer caracter hasta que sea espacio, tabulado o enter	
-				li $t2 9
-				beq $t3 $t2 fin
-				li $t2 13
-				beq $t3 $t2 fin
-				li $t2 20
-				beq $t3 $t2 fin
+irFinalPalabra:	#leer caracter hasta que sea espacio, tabulado o enter	
+				li $t3 0x9
+				beq $t1 $t3 fin
+				li $t3 0x13
+				beq $t1 $t3 fin
+				li $t3 0x20
+				beq $t1 $t3 fin
 				
 			#leemos otro caracter
 				move $a0 $s0
@@ -133,7 +139,7 @@ irFinalPalabra:		#leer caracter hasta que sea espacio, tabulado o enter
 				li $a2 1
 				li $v0 14
 				syscall
-				lb $t3 buffer
+				lb $t1 buffer
 				
 				b irFinalPalabra
 fin:				jr $ra		
